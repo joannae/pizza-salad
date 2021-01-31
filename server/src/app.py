05edb73683d5ac63789865d7e4091183f2713804
joannae <joanna.m.eriksson@gmail.com>
2021-01-31 16:10:15 +0100

    bound application to correct port

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,5 @@
 # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
+# https://medium.com/firebase-developers/hosting-flask-servers-on-firebase-from-scratch-c97cfb204579
 
 # Running with flask
 # $ python3 -m venv pizza_env
@@ -8,6 +9,7 @@
 # $ flask run
 
 from flask import Flask, render_template, request
+import os
 import pizza_salad
 
 app = Flask(__name__)
@@ -20,3 +22,6 @@ def index():
         return render_template('receipe.html', ingredients=ingredients)
     else:
         return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
